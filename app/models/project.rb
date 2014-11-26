2efa3716883a2f8982c520464c2cbce1fd1e0a9a
fliz <flizarthanon@gmail.com>
2014-11-26 10:39:18 -0800

    add categories and category index

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ActiveRecord::Base
-  validates :title, :user, presence: true
+  validates :title, :user, :category, presence: true
   validates :title, uniqueness: true
 
   belongs_to :user
+  belongs_to :category
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SatoshiFunder::Application.routes.draw do
   get "/start", to: "static_pages#start"
+  get "/discover", to: "categories#index"
   get "/projects/all", to: "projects#all"
   
   root "static_pages#root"
@@ -7,4 +8,5 @@ SatoshiFunder::Application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users
   resources :projects
+  resources :categories, only: [:index, :show]
 end
